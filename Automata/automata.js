37c9fbc1f44cfa212a35fde4b26edb1259d03370
YanaMun <123965036+YanaMun@users.noreply.github.com>
2024-12-23 23:34:32 +0500

    Update automata.js

--- Automata/automata.js
+++ Automata/automata.js
@@ -1,19 +1,15 @@
 function automata(text, pattern) {
     // Инициализация статистики
-    const stats = {
-        executionTime: 0,      // Время выполнения поиска
-        matches: [],           // Индексы всех совпадений
-        exactMatches: 0,       // Количество точных совпадений
-        collisions: 0,         // Количество коллизий
-        characterComparisons: 0 // Количество сравнений символов
-    };
+    let statistic = {'Time': 0, 'Indices': 0};
+    let indices = [];
+    let pointer = 0;
 
     let textLength = text.length;			  // Длина текста
     let patternLength = pattern.length;       // Длина подстроки
     let alph = new Array();                   // Переменная для мощности алфавита
 
     // Время начала поиска
-    const startTime = performance.now();
+    let startTime = performance.now();
 
     // Определение алфавита  шаблона
     for (let i = 0; i < patternLength; i++) {
@@ -43,22 +39,20 @@ function automata(text, pattern) {
     // Выполнение поиска
     let state = 0; // Текущее состояние автомата (где просматриваем подстроку на данный момент)
     for (let i = 0; i < textLength; i++) {
-        stats.characterComparisons++; // Увеличение счётчик сравнений символов
-
         if (text.charAt(i) in alph) {
             state = del[state][text.charAt(i)];
         } else {
             state = 0; // Сброс состояния при несоответствии
-            stats.collisions++; // Увеличение счётчик коллизий
         }
 
         if (state === patternLength) {
-            stats.matches.push(i - patternLength + 1); // Запись индекса совпадения
-            stats.exactMatches++; // Увеличение количество точных совпадений
+            indices[pointer]=(i-patternLength+1);
+            pointer++;
         }
     }
 
-    stats.executionTime = performance.now() - startTime; // Вычисление времени выполнения
+    statistic['Time'] = performance.now() - startTime;
+    statistic['Indices'] = indices;
 
 	// Вывод таблицы переходов
 	console.log("The transition table:");
@@ -71,14 +65,15 @@ function automata(text, pattern) {
     }
 	
     console.log("Statistics:")
-	return stats; //Возвращение статистики
+	return statistic; //Возвращение статистики
 }
 
 try {
     const fs = require('fs');
     let inputText = fs.readFileSync('input.txt', 'utf8');
+    let string = inputText.toString();
 
-    console.log(automata(inputText, "кол"));
+    console.log(automata(string, "кол"));
 } catch (error) {
     console.log("Возникла ошибка:");
     console.log(error.message);

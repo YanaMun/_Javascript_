a8e3b275e416199b4fa2c38af154cdec787c1dc4
YanaMun <123965036+YanaMun@users.noreply.github.com>
2024-12-25 12:24:05 +0500

    Update Entropy.js

--- Entropy/Entropy.js
+++ Entropy/Entropy.js
@@ -5,6 +5,8 @@ let inputFile = args[2];
 let input = fs.readFileSync(inputFile, 'utf8');
 let alph = new Object();
 let inLength = input.length;
+
+// Подсчитывается количество каждого различного элемента.
 for(let i=0;i<inLength;i++) {
     if(alph[input.charAt(i)])
         alph[input.charAt(i)] ++
@@ -13,12 +15,13 @@ for(let i=0;i<inLength;i++) {
 }
 
 let alphPower=0;
-
+// Вычисляется мощность алфавита и каждое количество различного символа делится на общее количество символов (длину строки).
 for(let i in alph) {
 	alphPower++;
     alph[i]/=inLength;
 }
 
+// Вычисление энтропии
 let entropy=0;
 if (alphPower>1) {
 	for(let i in alph)
